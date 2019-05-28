1e252cb797a9f5f701da2563c893e46bd8693df9
任跃兵 <cnryb@live.cn>
2019-05-28 18:08:53 +0800

    添加项目文件。

---
+++ IOPieplinesTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IOPieplinesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
